a00258bf4abbc84ac6c278f308648f9181c49898
dadderio <dadderio79@outlook.com>
2023-11-24 16:42:02 -0300

    update

--- Portal da Pro/src/components/CardNews.js
+++ Portal da Pro/src/components/CardNews.js
@@ -1,53 +1,48 @@
-class CardNews extends HTMLElement{
-  constructor(){
+class CardNews extends HTMLElement {
+  constructor() {
     super();
-  
-    const shadow = this.attachShadow({mode: "open"});
-    shadow.innerHTML = "<h1>hello</h1>";
+
+    const shadow = this.attachShadow({ mode: 'open' });
+    //shadow.innerHTML = '<h1>hello</h1>';
     shadow.appendChild(this.build());
     shadow.appendChild(this.styles());
-
   }
 
-  build(){
-    const componentRoot = document.createElement("div");
-    componentRoot.setAttribute("class", "card");
+  build() {
+    const componentRoot = document.createElement('div');
+    componentRoot.setAttribute('class', 'card');
 
-    const cardLeft = document.createElement("div");
-    cardLeft.setAttribute("class", "card__left");
+    const cardLeft = document.createElement('div');
+    cardLeft.setAttribute('class', 'card__left');
 
-    const autor = document.createElement("span");
-    autor.textContent = "By "+ (this.getAttribute("autor") || " Anonymous");
+    const autor = document.createElement('span');
+    autor.textContent = 'By ' + (this.getAttribute('autor') || ' Anonymous');
 
-    const linkTitle = document.createElement("a");
-    linkTitle.textContent = this.getAttribute("title");
-    linkTitle.href = this.getAttribute("url");
+    const linkTitle = document.createElement('a');
+    linkTitle.textContent = this.getAttribute('title');
+    linkTitle.href = this.getAttribute('url');
 
-    const newsContent = document.createElement("p");
-    newsContent.textContent = this.getAttribute("content");
+    const newsContent = document.createElement('p');
+    newsContent.textContent = this.getAttribute('content');
 
     cardLeft.appendChild(autor);
     cardLeft.appendChild(linkTitle);
     cardLeft.appendChild(newsContent);
 
-    const cardRight = document.createElement("div");
-    cardRight.setAttribute("class", "card__right");
-    const newsImage = document.createElement("img");
-    newsImage.src = "/assets/congratulations-you-did-test-very-well.jpg";
+    const cardRight = document.createElement('div');
+    cardRight.setAttribute('class', 'card__right');
+    const newsImage = document.createElement('img');
+    newsImage.src = this.getAttribute('photo') || 'assets/foto-default.jpeg';
+    newsImage.alt = 'professora preta lecionando para ensino infantil';
     cardRight.appendChild(newsImage);
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight);
 
-
-
-
     return componentRoot;
   }
 
-  styles(){}
-
-
+  styles() {}
 }
 
 customElements.define('card-news', CardNews);
