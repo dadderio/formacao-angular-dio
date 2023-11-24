c614f3c5bfcba14df4ffc59124efffb22c144184
dadderio <dadderio79@outlook.com>
2023-11-23 23:17:20 -0300

    update: incluindo arquivos

---
+++ Portal da Pro/src/components/CardNews.js
@@ -0,0 +1,53 @@
+class CardNews extends HTMLElement{
+  constructor(){
+    super();
+  
+    const shadow = this.attachShadow({mode: "open"});
+    shadow.innerHTML = "<h1>hello</h1>";
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+
+  }
+
+  build(){
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__left");
+
+    const autor = document.createElement("span");
+    autor.textContent = "By "+ (this.getAttribute("autor") || " Anonymous");
+
+    const linkTitle = document.createElement("a");
+    linkTitle.textContent = this.getAttribute("title");
+    linkTitle.href = this.getAttribute("url");
+
+    const newsContent = document.createElement("p");
+    newsContent.textContent = this.getAttribute("content");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__right");
+    const newsImage = document.createElement("img");
+    newsImage.src = "/assets/congratulations-you-did-test-very-well.jpg";
+    cardRight.appendChild(newsImage);
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+
+
+
+    return componentRoot;
+  }
+
+  styles(){}
+
+
+}
+
+customElements.define('card-news', CardNews);
